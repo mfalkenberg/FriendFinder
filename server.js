61f4720f29a0a88bd3ab3a2e2eaad51ce1c84780
Maggie Falkenberg <mf1306@yahoo.com>
2018-02-25 14:06:37 -0800

    .gitignore fixed

--- server.js
+++ server.js
@@ -1,15 +1,7 @@
-// ==============================================================================
-// DEPENDENCIES
-// Series of npm packages that we will use to give our server useful functionality
-// ==============================================================================
 
 var express = require("express");
 var bodyParser = require("body-parser");
 
-// ==============================================================================
-// EXPRESS CONFIGURATION
-// This sets up the basic properties for our express server
-// ==============================================================================
 
 // Tells node that we are creating an "express" server
 var app = express();
@@ -25,22 +17,14 @@ var PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// ================================================================================
 // ROUTER
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-// ================================================================================
 require("./routing/apiRoutes")(app);
-// // var apiRoutes = require("./routes/apiRoutes")
-// // console.log(apiRoutes); => [Function]
-// // apiRoutes(app);
+
 require("./routing/htmlRoutes")(app);
 
-// =============================================================================
-// LISTENER
 // The below code effectively "starts" our server
-// =============================================================================
-
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
