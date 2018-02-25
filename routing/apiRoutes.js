2507bac1b58653aa1165bcc0bdd614f5ee2b481e
Maggie Falkenberg <mf1306@yahoo.com>
2018-02-25 15:24:44 -0800

    updated .gitignore

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -4,16 +4,11 @@
 
 var friends = require("../data/friends");
 
-// ===============================================================================
-// ROUTING
-// ===============================================================================
-
+// Routing
 module.exports = function(app) {
   // API GET Requests
   // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
+  
 
   app.get("/api/friends", function(req, res) {
     res.json(friends);
@@ -21,30 +16,23 @@ module.exports = function(app) {
 
   // API POST Requests
   // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
 
   app.post("/api/friends", function(req, res) {
-  	console.log(req.body['scores[]']);
+  	// var scores take user input scores
   	var scores = req.body['scores[]'];
   	var currentMin = 1000;
   	var currentMinIndex = -1; 
   	for (var i = 0; i < friends.length; i++) {
-  		console.log("***" + i);
+  		// otherScores take already in data scores
   		var otherScores = friends[i].scores;
-  		console.log(otherScores);
   		var difference = 0;
   		for (var j = 0; j < scores.length; j++) {
   			difference = difference + Math.abs(scores[j] - otherScores[j]);
   		}
-  		console.log("difference: " + difference);
+
   		if (difference < currentMin) {
   			currentMinIndex = i;
   			currentMin = difference;
-  			console.log("bfhjs");
   		}
   	}
   	res.json(friends[currentMinIndex]);
